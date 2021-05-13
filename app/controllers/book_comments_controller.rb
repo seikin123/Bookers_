b1be9da39c94ee2e808ad63c04c59481f3b998f7
ヨシダタイキ <abc521fjr7@gmail.com>
2021-05-13 22:28:36 +0900

    [add]createアクション

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,21 @@
+class BookCommentsController < ApplicationController
+
+  def create
+    @book = Book.find(params[:book_id])
+    @book_comment = @book.book_comments.build(book_comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comment.save
+    render :index
+  end
+
+  def destroy
+    @book_comment = BookComment.find(params[:id])
+    @book_comment.destroy
+    render :index
+    # redirect_to books_path
+  end
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment, :book_id, :user_id)
+  end
+end
