da00d802bc5d51a5eb0eeaa225ca08d9d2648765
ヨシダタイキ <abc521fjr7@gmail.com>
2021-05-15 15:23:27 +0900

    [add]コメント機能のresorces

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+  end
+end
+
+resources :relationships, only: [:create, :destroy]
   root to: "homes#top"
   get "/home/about" => "homes#about"
   
   resources :books do
+    # いいね機能
     resource :favorites, only: [:create, :destroy]
+    # コメント機能
+    resources :book_comments, only: [:create, :destroy]
   end
 
 end
