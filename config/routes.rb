ccdc5926c1ccd5cfd25494e6a4f5909aca6a8116
ヨシダタイキ <abc521fjr7@gmail.com>
2021-05-21 19:25:16 +0900

    [update]follow機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users do
-    member do
-      get :following, :followers
-  end
-end
+  resources :users 
 
 resources :relationships, only: [:create, :destroy]
   root to: "homes#top"
